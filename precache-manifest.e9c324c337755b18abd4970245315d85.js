648acfc36cc1bbc4e4edc46df450212b2429ee7a
Admin <raju.chapagain@outlook.com>
2019-12-08 16:43:09 +1100

    Updates

---
+++ precache-manifest.e9c324c337755b18abd4970245315d85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13be22c2970b93680d221726efa4bbf",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "f7590d9c76f618937b0d",
+    "url": "/monsters/static/css/main.234ae403.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "f7590d9c76f618937b0d",
+    "url": "/monsters/static/js/main.4da29ade.chunk.js"
+  },
+  {
+    "revision": "852058801be7c79d6b02",
+    "url": "/monsters/static/js/runtime-main.98fd8d22.js"
+  }
+]);
